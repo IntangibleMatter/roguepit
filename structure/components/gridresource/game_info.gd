5475709260ecb674e2b1ee1f62183fd7f13c19ee
IntangibleMatter <47702559+IntangibleMatter@users.noreply.github.com>
2023-10-23 00:51:44 -0700

    sdhufk

--- structure/components/gridresource/game_info.gd
+++ structure/components/gridresource/game_info.gd
@@ -11,6 +11,22 @@ func update() -> void:
 		for obj in objects[pos]:
 			obj.update()
 
+
+func check_position(position: Vector2i, type: GameObject.ACTOR_TYPE) -> GameObject:
+	if objects.has(position):
+		if not objects[position].is_empty():
+			for obj in objects[position]:
+				if obj.type == type:
+					return obj
+	return null
+
+
+func clean_empty_tiles() -> void:
+	for tile in objects:
+		if objects[tile].is_empty():
+			objects.erase(tile)
+
+
 func check_tile(position: Vector2i) -> int:
 	var tile := tilemap.get_cell_tile_data(0, position)
 	if tile:
